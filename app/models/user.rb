873348777635d6c05264fdfe08e9c06589d3bd98
Carter Leffen <carter@carterleffen.com>
2012-04-02 17:50:24 -0500

    Finish sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,11 @@
 class User < ActiveRecord::Base
   attr_accessible :name, :email, :password, :password_confirmation
   has_secure_password
+  validates_presence_of :password, :on => :create
+
+
+  before_save :create_remember_token
+
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
 
@@ -21,5 +26,13 @@ class User < ActiveRecord::Base
                     uniqueness: { case_sensitive: false }
   validates :password, length: { minimum: 6 }
   validates :password_confirmation, presence: true
+
+
+
+  private
+
+    def create_remember_token
+      self.remember_token = SecureRandom.urlsafe_base64
+    end
 
 end
