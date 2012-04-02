8fa51ca7745c39523348f67c5c9224d5944636a7
Carter Leffen <carter@carterleffen.com>
2012-04-02 08:03:51 -0500

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Sample::Application.routes.draw do
-  get "users/new"
+
+  resources :users
 
   root to: 'static_pages#home'
 
---
+++ spec/factories.rb
@@ -0,0 +1,10 @@
+FactoryGirl.define do
+
+  factory :user do
+    name      "Carter Leffen"
+    email     "cleffen@gmail.com"
+    password  "password"
+    password_confirmation "password"
+  end
+
+end
